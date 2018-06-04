d975a641949022e5dff5dc0b16f13ee20af31858
Jonas Pedersen <Jonas@Jonas-MacBook-Pro.local>
2018-06-04 02:57:14 +0200

    DONE!

--- js/nav.js
+++ js/nav.js
@@ -3,6 +3,18 @@ async function hentNav() {
     let nav = await navFil.text();
 
     document.querySelector("[data-nav]").innerHTML = nav;
-    document.querySelector("[data-burger]").innerHTML = nav;
+
 }
 window.addEventListener("load", hentNav);
+
+
+
+
+async function hentNavBurger() {
+    let navFil = await fetch("template_nav_burger.html");
+    let nav = await navFil.text();
+
+    document.querySelector("[data-burger]").innerHTML = nav;
+
+}
+window.addEventListener("load", hentNavBurger);
